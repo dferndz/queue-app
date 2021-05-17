00108c34be340e75e6a694de22bf4572469df115
Daniel Fernandez <39488892+dferndz@users.noreply.github.com>
2021-05-17 01:56:18 -0500

    background

--- src/features/findQueue/index.tsx
+++ src/features/findQueue/index.tsx
@@ -18,10 +18,18 @@ import useQueues from "./useQueues";
 const useStyles = makeStyles((theme) => ({
   container: {
     height: "100vh",
+    position: "absolute",
+    top: "0",
   },
   header: {
     backgroundColor: "#bf5700",
     color: "white",
+  },
+  bgImage: {
+    backgroundImage: "url(https://wallpaperaccess.com/full/1566134.jpg)",
+    backgroundPosition: "top center",
+    filter: "blur(6px)",
+    height: "100vh",
   },
 }));
 
@@ -48,56 +56,59 @@ const QueueFinder = () => {
   }, [queueId]);
 
   return (
-    <Grid
-      className={classes.container}
-      container
-      alignContent="center"
-      justify="center"
-    >
-      <Grid item xs={10} sm={6} md={6} lg={4}>
-        <Card>
-          <CardHeader className={classes.header} title="Find a queue" />
-          <CardContent>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <TextField
-                  onChange={handleChange}
-                  label="Queue id"
-                  variant="outlined"
-                  value={queueId}
-                  fullWidth
-                />
-                {data && data.length > 0 && !selected && (
-                  <SearchResults data={data} onSelect={handleSelect} />
-                )}
+    <div>
+      <div className={classes.bgImage}></div>
+      <Grid
+        className={classes.container}
+        container
+        alignContent="center"
+        justify="center"
+      >
+        <Grid item xs={10} sm={6} md={6} lg={4}>
+          <Card>
+            <CardHeader className={classes.header} title="Find a queue" />
+            <CardContent>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <TextField
+                    onChange={handleChange}
+                    label="Queue id"
+                    variant="outlined"
+                    value={queueId}
+                    fullWidth
+                  />
+                  {data && data.length > 0 && !selected && (
+                    <SearchResults data={data} onSelect={handleSelect} />
+                  )}
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    disabled={!selected}
+                    variant="contained"
+                    color="secondary"
+                    onClick={handleNext}
+                  >
+                    Next
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <Button
-                  disabled={!selected}
-                  variant="contained"
-                  color="secondary"
-                  onClick={handleNext}
-                >
-                  Next
+            </CardContent>
+            <CardActions>
+              <Link href="/school">
+                <Button size="small" color="secondary">
+                  Register school
                 </Button>
-              </Grid>
-            </Grid>
-          </CardContent>
-          <CardActions>
-            <Link href="/school">
-              <Button size="small" color="secondary">
-                Register school
-              </Button>
-            </Link>
-            <Link href="/instructor">
-              <Button size="small" color="secondary">
-                Instructor account
-              </Button>
-            </Link>
-          </CardActions>
-        </Card>
+              </Link>
+              <Link href="/instructor">
+                <Button size="small" color="secondary">
+                  Instructor account
+                </Button>
+              </Link>
+            </CardActions>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 };
 
